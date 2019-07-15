9f7f8abb51c945d41c46b27b86211d55b11c31c3
Lateefat Amuda <teepha06@gmail.com>
2019-07-15 23:28:52 +0100

    - add hamburger menu - add mobile nav bar - style hamburger menu and mobile navbar

--- shared.js
+++ shared.js
@@ -2,19 +2,43 @@ const backDrop = document.querySelector(".backdrop");
 const modalPopup = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan .button");
 const closeModalButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+// console.dir(backDrop.getElementsByClassName.backgroundImage);
+// console.dir(backDrop.getElementsByClassName["background-image"]);
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    modalPopup.style.display = "block";
-    backDrop.style.display = "block";
+    // modalPopup.style.display = "block";
+    // backDrop.style.display = "block";
+    // modalPopup.className = ".open";  //This will override the complete class list
+    modalPopup.classList.add("open");
+    backDrop.classList.add("open");
   });
 }
 
-backDrop.addEventListener("click", closeModal);
-closeModalButton.addEventListener("click", closeModal);
+backDrop.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  closeModal();
+});
 
-function closeModal() {
-  modalPopup.style.display = "none";
-  backDrop.style.display = "none";
+if (closeModalButton) {
+  closeModalButton.addEventListener("click", closeModal);
 }
 
+function closeModal() {
+  if (modalPopup) {
+    modalPopup.classList.remove("open");
+  }
+  backDrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.classList.add("open");
+  backDrop.classList.add("open");
+});
+
+// toggleButton.addEventListener("click", function() {
+//   mobileNav.classList.remove("open");
+// });
